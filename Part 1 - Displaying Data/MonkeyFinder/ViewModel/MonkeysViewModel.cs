019c963692dd8ad79680a79cbdf993d1f96b938f
amaasik <75305698+amaasik@users.noreply.github.com>
2023-05-18 16:44:14 +0300

    MonkeysViewModel

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -3,14 +3,18 @@ namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
-    MonkeyService monkeyService;
+   
     public ObservableCollection<Monkey> Monkeys { get; } = new();
+    MonkeyService monkeyService;
     IConnectivity connectivity;
+    IGeolocation geolocation;
 
-    public MonkeysViewModel(MonkeyService monkeyService)
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
         Title = "Monkey Finder";
         this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+        this.geolocation = geolocation;
     }
     [RelayCommand]
     async Task GoToDetailsAsync(Monkey monkey)
@@ -57,6 +61,39 @@ public partial class MonkeysViewModel : BaseViewModel
         {
             IsBusy = false;
         }
+        [RelayCommand]
+        async Task GetClosestMonkey()
+        {
+            if (IsBusy || Monkeys.Count == 0)
+                return;
 
+            try
+            {
+                // Get cached location, else get real location.
+                var location = await geolocation.GetLastKnownLocationAsync();
+                if (location == null)
+                {
+                    location = await geolocation.GetLocationAsync(new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30)
+                    });
+                }
+
+                // Find closest monkey to us
+                var first = Monkeys.OrderBy(m => location.CalculateDistance(
+                    new Location(m.Latitude, m.Longitude), DistanceUnits.Miles))
+                    .FirstOrDefault();
+
+                await Shell.Current.DisplayAlert("", first.Name + " " +
+                    first.Location, "OK");
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to query location: {ex.Message}");
+                await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+            }
+        }
     }
 }
